642d002bd855aa1989ff64b4ebd0629d655eaf38
deSpooky <phurrde@gmail.com>
2025-09-18 21:33:24 +0300

    valibot schema update

--- shared/schema/src/feedback_type.ts
+++ shared/schema/src/feedback_type.ts
@@ -1,6 +1,10 @@
-// TODO: rewrite as valibot schema
+import * as v from "valibot";
 
-export interface FeedbackTypeTable {
-  id: number;
-  title: "Пожелание" | "Замечание";
-}
+const idSchema = v.pipe(v.number(), v.integer(), v.minValue(1))
+
+export const feedbackTypeSchema = v.object({
+  id: idSchema,
+  title: v.picklist(["Пожелание", "Замечание"]),
+})
+
+export type FeedbackTypeTable = v.InferInput<typeof feedbackTypeSchema>
